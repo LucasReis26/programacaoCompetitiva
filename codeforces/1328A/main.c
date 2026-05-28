6a46f019781e4fe6312b5e37b8253b7b9caef173
Lucas Reis <dev.lucasreis26@gmail.com>
2026-05-28 15:59:50 -0300

    codeforces: 1328A concluido

--- codeforces/1328A/main.c
+++ codeforces/1328A/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+int getADivisible(int,int);
+
 int main(){
 	int n;
 	scanf("%d",&n);
 	for(int i = 0; i < n; i++){
 		int a,b;
 		scanf("%d %d",&a,&b);
-		printf("%d\n",a%b);
-		printf("%d %d\n",a,b);
+		printf("%d\n",getADivisible(a,b));
 	}
 	return 0;
+}
+
+int getADivisible(int a,int b){
+	if(a%b == 0) return 0;
+	else return b - (a%b);
 }
